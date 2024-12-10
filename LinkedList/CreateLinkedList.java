30028b446abee519613ab5d14e0011af606c1cc5
Gaurav <khatrigarima073@gmail.com>
2024-12-10 23:04:18 +0530

    create single linkedlist

---
+++ LinkedList/CreateLinkedList.java
@@ -0,0 +1,23 @@
+public class CreateLinkedList {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a=new Node(12);
+        Node b=new Node(78);
+        Node c=new Node(90);
+        Node d=new Node(55);
+
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=null;
+
+        System.out.print(a.data+" "+b.data+" "+c.data+" "+d.data);
+    }
+}
