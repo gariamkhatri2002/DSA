012da7c74c0aaf9b3cd9bd2e65fb9a8e7bf10a9a
Gaurav <khatrigarima073@gmail.com>
2025-01-02 23:40:05 +0530

    user defined pop method of stack

---
+++ Stack/PopMethod.java
@@ -0,0 +1,44 @@
+package Stack;
+
+public class PopMethod {
+    
+        public static class Stack {
+            int index = 0;
+            int a[] = new int[5];
+    
+            
+            public void push(int val) {
+                a[index] = val;
+                index++;
+            }
+    
+            public void display() {
+                for (int i = 0; i < index; i++) {
+                    System.out.print(a[i] + " ");
+                }
+            }
+    
+            int pop() {
+                if (index == 0) {
+                    System.out.println("empty stack");
+                    return -1;
+                } else {
+                    int top = a[index - 1];
+                    a[index - 1] = 0;
+                    index--;
+                    return top;
+                }
+            }
+        
+        }
+        public static void main(String[] args) {
+            Stack st = new Stack();
+            st.push(1);
+            st.push(2);
+            st.push(3);
+            
+            st.pop();
+            st.display();
+        }
+    
+}
